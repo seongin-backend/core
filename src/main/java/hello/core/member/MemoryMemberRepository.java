e7100306f8d72502f801297ff26c677b065bc701
seongin_backend <https://github.com/seongin-backend>
2022-06-08 13:23:51 +0900

    주문과 할인 자바만 써서 개발

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -11,7 +11,6 @@ public class MemoryMemberRepository implements MemberRepository {
     public void save(Member member) {
         store.put(member.getId(), member);
     }
-
     @Override
     public Member findById(Long memberId) {
         return store.get(memberId);
