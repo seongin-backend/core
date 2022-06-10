39b1625234a718827facf31e705e8a3f3d0a44a9
seongin_backend <https://github.com/seongin-backend>
2022-06-10 17:54:53 +0900

    깃 테스트

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
 	public static void main(String[] args) {
-
+		//git test
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
